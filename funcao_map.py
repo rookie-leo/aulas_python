450cadc17d392e0427eb9e921cd46be69aec8f7c
rookie-leo <proleonardopa@gmail.com>
2023-08-18 09:24:24 -0300

    Função map com lambda

--- funcao_map.py
+++ funcao_map.py
@@ -10,3 +10,14 @@ print(duplica(2))
 lista_mapeada = map(duplica, lista)
 
 print(list(lista_mapeada))
+
+# soma entre elementos de duas istas diferentes
+lista2 = [5, 6, 7, 8]
+
+print(list(map(lambda num1, num2: num1 + num2, lista, lista2)))
+
+# Passando para upper case
+lista_strings = ["lista", "de", "strings"]
+
+print(list(map(str.upper, lista_strings)))
+
